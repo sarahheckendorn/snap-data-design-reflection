ed8f57b261403cc662e1a70cfcdc6516e7e1d30b
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-24 08:25:57 -0600

    committed our morning snap challenge

---
+++ snap-challenge.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Snap Challenge on Data Design Reflections</title>
+	</head>
+	<body>
+		<h1>Sarah's Reflections</h1>
+		<div>
+			<p><strong>What is the most significant thing you have learned about object oriented?</strong></br>
+			That it is an effective way to run back end code, is easy to maintain (due to its minimalistic approach), and is incredibly powerful.</p>
+			<p><strong>What is the most significant thing you have learned about the effect of the frontend on the backend?</strong></br>
+				That in order for the backend to be used the way it's designed to, the front end needs to be able to reflect it clearly in an 'easy to use' concept</br>
+			for the end user. They need to work together and remember that each is important.</p>
+			<p><strong>Why is encapsulation so important?</strong></br>
+			Because it gives protection to the code you are working on so not just anyone can come in and change it.</p>
+			<p><strong>BONUS</strong></br>
+				If we had indeed been brought to England.... but really, the question is, African or European Swallow?</p>
+			<p>Though this class may be challenging, my stance remains that... 'tis but a flesh wound'.</p>
+		</div>
+	</body>
+</html>
